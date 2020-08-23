948dbde8f49dbbedf832de34be1c6b0792724ed3
Adam Konieczny <akonieczny094@gmail.com>
2020-08-23 20:16:45 +0200

    Fix time_ms and time_us

--- cornsnipps/time.py
+++ cornsnipps/time.py
@@ -4,7 +4,7 @@ import typing as tp
 __all__ = ['time_ms', 'time_us']
 
 
-def time_ms(as_float: bool = True) -> tp.Union[int, float]:
+def time_ms(as_float: bool = False) -> tp.Union[int, float]:
     """Convert current time to milliseconds.
 
     :param as_float: result should be float, default result is int
@@ -12,8 +12,8 @@ def time_ms(as_float: bool = True) -> tp.Union[int, float]:
     """
     _time_ms = time.time() * 1000
     if not as_float:
-        return _time_ms
-    return int(_time_ms)
+        return int(_time_ms)
+    return _time_ms
 
 
 def time_us(as_float: bool = False) -> tp.Union[int, float]:
@@ -27,5 +27,5 @@ def time_us(as_float: bool = False) -> tp.Union[int, float]:
     """
     _time_us = time.time() * 1000000
     if not as_float:
-        return _time_us
-    return int(_time_us)
+        return int(_time_us)
+    return _time_us
--- tests/test_time.py
+++ tests/test_time.py
@@ -3,12 +3,30 @@ from unittest import mock
 
 from cornsnipps.time import time_ms, time_us
 
+current_time = 13.37
+
 
 class TestTime(TestCase):
-    @mock.patch('time.time', return_value=1337)
+    @mock.patch('time.time', return_value=current_time)
     def test_time_ms(self, mock_time):
-        self.assertEqual(1337 * 1000, time_ms())
+        _current_time_ms = current_time * 1000
 
-    @mock.patch('time.time', return_value=1337)
+        _time_ms = time_ms()
+        self.assertEqual(int(_current_time_ms), _time_ms)
+        self.assertIsInstance(_time_ms, int)
+
+        _time_ms = time_ms(True)
+        self.assertEqual(_current_time_ms, _time_ms)
+        self.assertIsInstance(_time_ms, float)
+
+    @mock.patch('time.time', return_value=current_time)
     def test_time_us(self, mock_time):
-        self.assertEqual(1337 * 1000 * 1000, time_us())
+        _current_time_us = current_time * 1000 * 1000
+
+        _time_us = time_us()
+        self.assertEqual(int(_current_time_us), _time_us)
+        self.assertIsInstance(_time_us, int)
+
+        _time_us = time_us(True)
+        self.assertEqual(_current_time_us, _time_us)
+        self.assertIsInstance(_time_us, float)
